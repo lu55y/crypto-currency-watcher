5d8a2f6a6c16ab1aff89a6a2d9043ccb006281d2
yatsevich_p <mu11er100500@gmail.com>
2023-06-06 21:37:01 +0300

    06.06.23 working commit.Final testing. Added flyway migration.

--- src/main/java/com/example/crypto_currency_watcher/repository/CurrencyRepository.java
+++ src/main/java/com/example/crypto_currency_watcher/repository/CurrencyRepository.java
@@ -3,10 +3,8 @@ package com.example.crypto_currency_watcher.repository;
 import com.example.crypto_currency_watcher.model.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
     Optional<Currency> findBySymbol(String symbol);
-    List<Currency> findAllBySymbolIn (List<String> symbol);
 }
